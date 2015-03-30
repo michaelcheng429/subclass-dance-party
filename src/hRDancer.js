ef58351932097ce8a21929354a053f91a550bde3
Michael Cheng <cheng.c.mike@gmail.com>
2015-03-30 17:43:19 -0500

    hrdancer can move

--- src/hRDancer.js
+++ src/hRDancer.js
@@ -8,4 +8,20 @@ HRDancer.prototype.constructor = HRDancer;
 HRDancer.prototype.step = function(){
   Dancer.prototype.step.call(this);
   this.$node.fadeToggle();
+  var randY = $("body").height() * Math.random();
+  var randX = $("body").width() * Math.random(); 
+
+
+  setTimeout(function(){
+    Dancer.prototype.setPosition.call(this, randY, randX);
+  }.bind(this), 1000);
 };
+
+// var randY = $("body").height() / 10 * Math.random();
+//   var randX = $("body").width() / 10 * Math.random(); 
+
+
+//   setTimeout(function(){
+//     Dancer.prototype.setPosition.call(this, this.position().top + randY,
+//     this.position().left +  randX);
+//   }.bind(this), 1000);
