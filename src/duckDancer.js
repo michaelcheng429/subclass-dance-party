13b22c7b9d0edd0eda86e35301f3d7aa71fae738
Michael Cheng <cheng.c.mike@gmail.com>
2015-03-31 19:31:31 -0500

    hr logos grow again on mouseover

--- src/duckDancer.js
+++ src/duckDancer.js
@@ -2,6 +2,7 @@ var DuckDancer = function(top, left, timeBetweenSteps){
   HRDancer.call(this, top, left, timeBetweenSteps);
   this.$node.addClass('duck');
   this.$node.removeClass('hr');
+  
 };
 
 DuckDancer.prototype = Object.create(HRDancer.prototype);
